7f6c3a4e71eac386d7cc3ef609a92609fd064eb4
Gautam <gk9765551@gmail.com>
2023-01-21 16:11:10 +0530

    while loop

---
+++ while.dart
@@ -0,0 +1,10 @@
+void main() {
+  int no = 20; // Initialization Count will start from here.
+  
+  // This is the condition
+  while (no <= 30) {
+    print("Number is $no");
+    // This is the increment.
+    no++;
+  }
+}
