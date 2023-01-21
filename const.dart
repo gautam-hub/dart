3cafd2a6758cb9a78def944cc581cc179774ad52
Gautam <gk9765551@gmail.com>
2023-01-21 14:27:44 +0530

    list in const

--- const.dart
+++ const.dart
@@ -1,9 +1,19 @@
+
 void main() {
   print("i am dart");
-  
+
   // Like final we cant override the value of const also.
   const name = "Raman";
-  
+
   // But in consta we have to declare its value inline.
   print(name);
+
+  const names = [
+    "Ram",
+    "Raman",
+    "Shreyash",
+    "Jethalal",
+  ];
+
+   // names.add("Babita"); This will throw an error because after using const we cant add or edit the list. 
 }
