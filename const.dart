711fb0b0bafe2dd3969382cd6e8037f0a999dfdc
Gautam <gk9765551@gmail.com>
2023-01-21 14:20:03 +0530

    constant in dart

---
+++ const.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("i am dart");
+  
+  // Like final we cant override the value of const also.
+  const name = "Raman";
+  
+  // But in consta we have to declare its value inline.
+  print(name);
+}
