264194d864586c30f44355566044d44f33018e84
Gautam <gk9765551@gmail.com>
2023-01-21 15:56:37 +0530

    for loop

---
+++ for.dart
@@ -0,0 +1,8 @@
+void main() {
+  // i=5; is initialization. which means the process will start from 5.
+  // i<10; is condition. Till here the loop will run.
+  // a++ is incrementation.
+  for (var i = 5; i < 10; i++) {
+    print("Hello World");
+  }
+}
