696ad537d83ae7bcdea1b8dce9f7d7e981105f3d
Gautam <gk9765551@gmail.com>
2023-01-21 14:17:37 +0530

    final

---
+++ final.dart
@@ -0,0 +1,11 @@
+void main() {
+  print("I am dart");
+
+  final name;
+  name = "Raman";
+
+  // We cant override/change the final value again, we can define the value of final later on also.
+  //  name = "Ram"; (It will give an error)
+
+  print(name);
+}
